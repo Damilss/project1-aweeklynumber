e9deda55d2652a0f39421323d5d96d67c34bf0db
Emilio Scott <milii76@outlook.com>
2026-04-06 09:08:28 -0700

    fix redundant static and public main and sample code

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-public static void main() {
+void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("Hello and welcome!"));
