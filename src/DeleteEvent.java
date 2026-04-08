4c1c18cb8b93c8dbe204c411b6ad2fe7d5504bba
Emilio Scott <milii76@outlook.com>
2026-04-07 19:52:18 -0700

    style src for better readbility and context

---
+++ src/DeleteEvent.java
@@ -0,0 +1,35 @@
+//DeleteEvent.java
+//Weekly calendar Structure 
+/*
+ArrayList < ArrayList< String >> = new ArrayList<>(); 
+
+for (int i = 0; i < 7; i++){
+    arrayList <String> day = New ArrayList<>; 
+    for (j = 0; < 24; i++){
+        String event = (null);
+    }
+}
+*/
+
+//visual aid for weeklyCalendar 
+/*
+[
+["Math class at 10:00am, Gym at 2:00pm"],
+[],
+["Dance class at 9:00am","CS class at 11:00am"],
+["Club meeting at 5:00pm"],
+[],
+[],
+["Go to the beach at 10:00am"],
+]
+*/
+
+import java.util.ArrayList;
+
+public class DeleteEvent {
+    ArrayList < ArrayList< String > > weeklyCalendar = new ArrayList<>();
+    String[][] calendar;
+    public void main() { 
+        
+    }
+}
